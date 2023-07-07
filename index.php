7e912563dc93a863b532f1101f9e2a8068005436
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-07-06 22:09:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>.: CAMPO FUTEBOL :.</title>
+</head>
+<body>
+	<div id="campo">
+		<div id="linhaprincipal">
+			<div id="circulo-centro"></div>
+			<div id="circulo-central"></div>
+			<div id="linha-central"></div>
+			<div id="borda1"></div>
+			<div id="borda2"></div>
+			<div id="borda3"></div>
+			<div id="borda4"></div>
+		</div>
+	</div>
+</body>
+</html>
